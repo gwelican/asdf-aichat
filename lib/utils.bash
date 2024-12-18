295fe811b9332381bdf729f2186f697140cd97eb
Peter Varsanyi <superfly@gwelican.eu>
2024-12-17 22:18:54 -0800

    fix

--- lib/utils.bash
+++ lib/utils.bash
@@ -41,19 +41,18 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-
 	local platform
 	case "$OSTYPE" in
-		darwin*) platform="apple-darwin" ;;
-		linux*) platform="unknown-linux-musl" ;;
-		*) fail "Unsupported platform" ;;
+	darwin*) platform="apple-darwin" ;;
+	linux*) platform="unknown-linux-musl" ;;
+	*) fail "Unsupported platform" ;;
 	esac
 
 	local architecture
 	case "$(uname -m)" in
-		x86_64) architecture="x86_64" ;;
-		arm64) architecture="aarch64" ;; # no armv64 build :(
-		*) fail "Unsupported architecture" ;;
+	x86_64) architecture="x86_64" ;;
+	arm64) architecture="aarch64" ;; # no armv64 build :(
+	*) fail "Unsupported architecture" ;;
 	esac
 
 	# TODO: Adapt the release URL convention for aichat
